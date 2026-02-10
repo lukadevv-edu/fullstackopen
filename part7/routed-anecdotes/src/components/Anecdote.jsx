ad43bc58c19e834633b8ca360bda6d97cca1fdf0
Luka <lukawolf.dev@gmail.com>
2026-02-10 08:10:01 -0300

    Add routed-anecdotes app; update votes
    Add a new Vite + React example app at part7/routed-anecdotes (App, Anecdote, main, vite config, index.html, ESLint config, .gitignore, README, package.json). Add package-lock.json files for part7 and the subproject. Update part6/query-anecdotes/db.json to increment vote counts for several anecdotes.

---
+++ part7/routed-anecdotes/src/components/Anecdote.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+
+export function Anecdote({ anecdotes }) {
+  const id = useParams().id;
+
+  const anecdote = anecdotes.find((n) => n.id == id);
+
+  if (!anecdote) {
+    return <h1>404: Note does not exists!</h1>;
+  }
+
+  return (
+    <div>
+      <h2>
+        {anecdote.content} by {anecdote.author}
+      </h2>
+      <div>
+        <p>has {anecdote.votes} votes</p>
+        <p>
+          for more info see{" "}
+          <a href={anecdote.info} target="_blank" rel="noreferrer">
+            {anecdote.info}
+          </a>
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+}
